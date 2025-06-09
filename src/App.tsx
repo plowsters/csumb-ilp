c914062a2bfd980d908da79332e672ed22c1c06f
Bug Plowman <142384070+plowsters@users.noreply.github.com>
2025-06-09 01:40:41 +0000

    The routing problem was in my React BrowserRouter. No one could have predicted this

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/csumb-ilp">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/cst300" element={<CST300 />} />
